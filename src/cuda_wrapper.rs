1aeab02b9509520d39c2646a628dcf3cd3b95490
EtiNL <etienne.lanzeray@gmail.com>
2025-07-23 22:56:16 +0200

    merge changes on cuda_wrapper

--- src/cuda_wrapper.rs
+++ src/cuda_wrapper.rs
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-use cuda_driver_sys::{
-    cuCtxCreate_v2, cuCtxSynchronize, cuInit, cuLaunchKernel, cuMemAlloc_v2, cuMemcpyDtoH_v2, cuMemcpyHtoD_v2,
-    cuModuleGetFunction, cuModuleLoad, cuCtxDestroy_v2, cuModuleUnload, cuMemFree_v2,
-    CUdeviceptr, CUcontext, CUfunction, CUmodule, CUresult, cudaError_enum::CUDA_SUCCESS,
-};
-use std::any::Any;
-use std::ffi::CString;
-use std::ptr::null_mut;
-use std::mem;
-
-#[derive(Debug)]
-=======
 use cuda_driver_sys::*;
 use std::collections::HashMap;
 use std::ffi::CString;
@@ -19,7 +6,6 @@ use std::error::Error;
 use std::ffi::c_void;
 use crate::scene_composition::SdfObject;
 
->>>>>>> debbug_branch
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct dim3 {
@@ -75,62 +61,6 @@ impl CudaContext {
             let ptx_cstr = CString::new(ptx_path)?;
             check_cuda_result(cuModuleLoad(&mut module, ptx_cstr.as_ptr()), "cuModuleLoad")?;
 
-<<<<<<< HEAD
-            let mut function = null_mut();
-            let kernel_name = CString::new("computeSDF")?;
-            check_cuda_result(cuModuleGetFunction(&mut function, module, kernel_name.as_ptr()), "cuModuleGetFunction")?;
-
-            Ok(Self { context, module, function })
-        }
-    }
-
-    pub fn launch_kernel(&self, args: &mut [Box<dyn KernelArg>], width: u32, height: u32) -> Result<(), Box<dyn std::error::Error>> {
-        let grid_dim = dim3 { x: (width + 15) / 16, y: (height + 15) / 16, z: 1 };
-        let block_dim = dim3 { x: 16, y: 16, z: 1 };
-
-        unsafe {
-            let mut device_ptrs: Vec<CUdeviceptr> = Vec::new();
-            for arg in args.iter_mut() {
-                arg.allocate_on_device();
-                device_ptrs.push(arg.device_ptr());
-            }
-
-            let mut params: Vec<*mut std::ffi::c_void> = Vec::new();
-            for ptr in &device_ptrs {
-                params.push(ptr as *const CUdeviceptr as *mut std::ffi::c_void);
-            }
-
-            println!("Launching kernel with grid_dim: {:?}, block_dim: {:?}", grid_dim, block_dim);
-            for (i, param) in params.iter().enumerate() {
-                println!("Param {}: {:?}", i, param);
-            }
-
-            let result = cuLaunchKernel(
-                self.function,
-                grid_dim.x, grid_dim.y, grid_dim.z,
-                block_dim.x, block_dim.y, block_dim.z,
-                0, null_mut(), params.as_mut_ptr(), null_mut(),
-            );
-
-            if result != CUDA_SUCCESS {
-                eprintln!("cuLaunchKernel failed: {:?}", result);
-                return Err(Box::from(format!("cuLaunchKernel failed: {:?}", result)));
-            }
-
-            // Ensure kernel execution is complete
-            let sync_result = cuCtxSynchronize();
-            if sync_result != CUDA_SUCCESS {
-                eprintln!("cuCtxSynchronize failed: {:?}", sync_result);
-                return Err(Box::from(format!("cuCtxSynchronize failed: {:?}", sync_result)));
-            }
-
-            for arg in args.iter_mut() {
-                arg.copy_to_host();
-            }
-        }
-        Ok(())
-    }
-=======
             Ok(Self {
                 context,
                 module,
@@ -340,7 +270,6 @@ impl CudaContext {
         }
         Ok(())
     }
->>>>>>> debbug_branch
 }
 
 
@@ -356,77 +285,6 @@ impl Drop for CudaContext {
     }
 }
 
-<<<<<<< HEAD
-pub trait KernelArg: Any {
-    fn allocate_on_device(&mut self);
-    fn device_ptr(&self) -> CUdeviceptr;
-    fn copy_to_host(&mut self);
-    fn as_any(&self) -> &dyn Any;
-}
-
-pub struct DeviceBuffer<T> {
-    host_data: Vec<T>,
-    device_ptr: CUdeviceptr,
-}
-
-impl<T> DeviceBuffer<T> {
-    pub fn new(data: Vec<T>) -> Self {
-        Self {
-            host_data: data,
-            device_ptr: 0,
-        }
-    }
-
-    pub fn get_host_data(&self) -> &Vec<T> {
-        &self.host_data
-    }
-
-    pub fn get_host_data_mut(&mut self) -> &mut Vec<T> {
-        &mut self.host_data
-    }
-}
-
-impl<T: 'static> KernelArg for DeviceBuffer<T> where T: Copy + 'static {
-    fn allocate_on_device(&mut self) {
-        let size = self.host_data.len() * mem::size_of::<T>();
-        unsafe {
-            let result = cuMemAlloc_v2(&mut self.device_ptr, size);
-            check_cuda_result(result, "cuMemAlloc_v2").expect("Failed to allocate device memory");
-
-            let result = cuMemcpyHtoD_v2(self.device_ptr, self.host_data.as_ptr() as *const _, size);
-            check_cuda_result(result, "cuMemcpyHtoD_v2").expect("Failed to copy memory to device");
-
-            println!("Allocated {} bytes on device at {:?}", size, self.device_ptr);
-        }
-    }
-
-    fn device_ptr(&self) -> CUdeviceptr {
-        self.device_ptr
-    }
-
-    fn copy_to_host(&mut self) {
-        let size = self.host_data.len() * mem::size_of::<T>();
-        unsafe {
-            let result = cuMemcpyDtoH_v2(self.host_data.as_mut_ptr() as *mut _, self.device_ptr, size);
-            check_cuda_result(result, "cuMemcpyDtoH_v2").expect("Failed to copy memory from device");
-
-            println!("Copied {} bytes from device at {:?}", size, self.device_ptr);
-        }
-    }
-
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-}
-
-impl<T> Drop for DeviceBuffer<T> {
-    fn drop(&mut self) {
-        unsafe {
-            cuMemFree_v2(self.device_ptr);
-        }
-    }
-}
-=======
 // Gère le buffer GPU qui contient la scène.
 pub struct SceneBuffer {
     d_ptr:    CUdeviceptr,
@@ -476,4 +334,3 @@ impl Drop for SceneBuffer {
         let _ = CudaContext::free_device_memory(self.d_ptr);
     }
 }
->>>>>>> debbug_branch
